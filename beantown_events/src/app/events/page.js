2abc2235a4127f3153456ce0d023c46dc0670de1
AndersonBlanco <andersonblanco913@gmail.com>
2026-04-29 19:22:05 -0400

    Added empty, useless file at directory /events to avoid build error for vercel deployment

---
+++ beantown_events/src/app/events/page.js
@@ -0,0 +1,26 @@
+//Anderson 
+//Empy file, uselesss
+'use client';
+import { createCheckoutSessionAction } from "../../libs/stripe/stripe";
+
+import "./index.css"
+import "../globals.css"
+import Header from "../components/header/header";
+import { handleCheckout } from "@/libs/stripe/checkoutHandlerer";
+import { usePathname } from "next/navigation";
+
+export default function Events() {
+  //js logic: 
+  
+
+  //html code below
+  return (
+      <div className="eventsContainer">
+    
+
+      
+      </div>
+     
+
+  );
+}
